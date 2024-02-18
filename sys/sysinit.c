736631262be0007f6f97a833773052eb326a06f8
Agnosto Dvonik <dvonik@rslp.org>
2024-02-17 23:07:20 -0500

    put into a testable state

--- sys/sysinit.c
+++ sys/sysinit.c
@@ -4,9 +4,5 @@ void
 sysinit(void)
 {
 	vga_clearscreen();
-	vga_write("fish stick.", 10, 0x0F);
-	for (s16 i = 0; i <= 2000; i += 20) {
-		vga_write("Om Smart!", 9, 0x0F);
-		vga_write("Om Smarter!", 11, 0x0F);
-	} 
+	vga_write("fish stick.", 10, 0x07);
 }
