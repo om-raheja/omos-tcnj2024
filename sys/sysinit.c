a5822482db6c0c55ca088096532f9ab7dc3ff678
MonkeyGG2 <spinosaurus882@gmail.com>
2024-02-18 11:03:37 -0500

    printf itoa utoa

--- sys/sysinit.c
+++ sys/sysinit.c
@@ -11,7 +11,7 @@ sysinit(void)
 	for (s16 i = 0; i < 100; i++) {
 		vga_write(howdy, strlen(howdy), 0x1f);
 		vga_write(howday, strlen(howday), 0x0f);
-	} 
+	}
 
 	vga_write("sid", 3, 0x07);
 	vga_write("did", 3, 0x07);
@@ -47,4 +47,6 @@ sysinit(void)
 "bob", 2003, 0x0F);
 	vga_write("indian", 6, 0x07);
 	vga_write("indian", 6, 0x07);
+
+	printf("%s%x", "Omh", 0x2a);
 }
