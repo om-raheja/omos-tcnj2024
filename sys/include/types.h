2b3342238c8db28bf9a925a3de7171e3d6f58861
Agnosto Dvonik <dvonik@rslp.org>
2024-02-17 23:03:44 -0500

    added more types and useful bool macros

--- sys/include/types.h
+++ sys/include/types.h
@@ -1,13 +1,19 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-#define NULL				0x0
+#define NULL			0x00
+
+#define TRUE			0
+#define FALSE			1
 
 typedef	unsigned int		u32;
 typedef unsigned short		u16;
 typedef unsigned char		u8;
-typedef	int					s32;
-typedef short				s16;
-typedef char				s8;
+typedef	int			s32;
+typedef short			s16;
+typedef char			s8;
+
+typedef unsigned int		size_t;
+typedef int			ssize_t;
 
 #endif
