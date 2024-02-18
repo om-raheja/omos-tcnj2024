0892eb841c6400e74c10406edb7e234ab1843d01
spikey <spikey@rslp.org>
2024-02-18 11:14:38 -0500

    memory thing

--- sys/sysinit.c
+++ sys/sysinit.c
@@ -6,13 +6,10 @@ void
 sysinit(void)
 {
 	char *howdy = "Om Smart.";
-	char *howday = "Om Smarter.";
 
 	vga_clearscreen();
 	vga_write("Starting omOS...", 16, 0x0F);
 
 	memory_init();
 
-	s8 *start = (s8 *) 0xb8fa0;
-	start[0] = 'h';
 }
